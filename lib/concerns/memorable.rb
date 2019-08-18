dc482f44c7c8a968f2c4c78b9fd32ed081eed20f
LaNice Powell <lanicemp@aol.com>
2019-08-18 21:47:22 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,16 +1,19 @@
 module Memorable
- module InstanceMethods
-   
- end 
- 
- module ClassMethods  
-  def reset_all
-    self.all.clear
-  end
 
-  def count
-    self.all.count
-  end
-end 
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end 
+ 
+  module ClassMethods  
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  end 
 
 end
